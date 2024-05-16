8ffc15260bd825614109f41e20e4814dda2ffc29
loren <lollifuss@gmail.com>
2024-05-16 14:59:47 +0200

    Migliorato lato UI del sign in e sign up e stesura codice per la gestione dei blank space e psw1 == psw2

--- app/src/main/java/com/example/lf_parkcar/MainActivity.kt
+++ app/src/main/java/com/example/lf_parkcar/MainActivity.kt
@@ -1,4 +1,23 @@
-package com.example.lf_parkcar
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import com.example.lf_parkcar.Login
+import com.example.lf_parkcar.R
 
-class MainActivity {
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val handler = Handler(Looper.getMainLooper())
+        handler.postDelayed({
+            // Avvia l'Activity di Login dopo 5 secondi
+            startActivity(Intent(this, Login::class.java))
+            // Chiudi l'attuale MainActivity
+            finish()
+        }, 5000)
+    }
 }
--- app/src/main/java/com/example/lf_parkcar/Register.kt
+++ app/src/main/java/com/example/lf_parkcar/Register.kt
@@ -1,14 +1,51 @@
 package com.example.lf_parkcar
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.lf_parkcar.databinding.ActivitySignUpBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class Register : AppCompatActivity() {
 
+    private lateinit var binding:ActivitySignUpBinding
+    private lateinit var firebaseAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
 
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        firebaseAuth=FirebaseAuth.getInstance()
+
+        binding.textView.setOnClickListener{
+            val intent = Intent(this, Login::class.java)
+        }
+
+        binding.btn.setOnClickListener{
+            val email=binding.emailEt.text.toString()
+            val psw=binding.passET.text.toString()
+            val psw2=binding.passET2.text.toString()
+
+            if(email.isNotEmpty()&& psw.isNotEmpty() && psw2.isNotEmpty()){
+                if(psw==psw2){
+                    firebaseAuth.createUserWithEmailAndPassword(email, psw).addOnCompleteListener {
+                        if(it.isSuccessful){
+                            val intent = Intent(this, Login::class.java)
+                        }else{
+                            Toast.makeText(this,it.exception.toString(),Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                }else{
+                    Toast.makeText(this, "Le password non corrispondono", Toast.LENGTH_SHORT).show()
+                }
+            }
+            else{
+                Toast.makeText(this, "Spazi vuoti non ammessi", Toast.LENGTH_SHORT).show()
+            }
+        }
 
     }
 }
